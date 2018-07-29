3cdee4fda097909a99b7ed199aa2a0fca3d25c61
Lonadels <lonadels@ya.ru>
2018-07-30 01:21:03 +0300

    Add application loader

--- src/app/modules/AppModule.php
+++ src/app/modules/AppModule.php
@@ -6,5 +6,24 @@ use std, gui, framework, app;
 
 class AppModule extends AbstractModule
 {
+    /** @var app\modules\Debug debug class */
+    public $debug;
+    
+    /** @var app\modules\Main main class */
+    public $main;
 
+    function __construct()
+    {    
+        $this->debug = new Debug;
+        $this->debug->log("Waiting for application create...");
+    
+        new Thread(function(){
+        
+            while(! Application::isCreated() );
+                       
+            $this->debug->log("Application was created.");
+            $this->main = new Main($GLOBALS['argv']);
+            
+        })->start();
+    }
 }
