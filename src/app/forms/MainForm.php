5a3d7cae46f925c8d79995660705eb58d4119d40
Lonadels <lonadels@ya.ru>
2018-08-05 00:47:23 +0300

    form move

--- src/app/forms/MainForm.php
+++ src/app/forms/MainForm.php
@@ -7,4 +7,12 @@ use std, gui, framework, app;
 class MainForm extends AbstractForm
 {
 
+    /**
+     * @event panel3.click-Left 
+     */
+    function doPanel3ClickLeft(UXMouseEvent $e = null)
+    {    
+        app()->shutdown();
+    }
+
 }
